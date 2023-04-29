dd4691145df9e526af4237025bd0a3c818494948
Gustavo Bispo <gbispo@ymail.com>
2023-04-29 01:32:11 -0300

    Add Data for frank-hertz experiment and a method for FFT Plots

--- Methods.py
+++ Methods.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+from scipy.signal import find_peaks, peak_widths
+from numpy.fft import fft
 import numpy as np
 
 def Fit(x, y, func=None, sigy=None, p0=None):    
@@ -28,7 +30,7 @@ def Fit(x, y, func=None, sigy=None, p0=None):
     
     return popt,pcov,r
 
-def Plot(x, y, sigy=None, func: 'function'=None, p0=None,
+def PlotFit(x, y, sigy=None, func: 'function'=None, p0=None,
          xlabel: str='X', ylabel: str='Y', log: bool=False, marker: str='s', 
          markersize: float=12, markeredge: str='k', markerface: str='none'):
     
@@ -39,7 +41,13 @@ def Plot(x, y, sigy=None, func: 'function'=None, p0=None,
                 
         ax[0].errorbar(x, y, sigy, fmt=marker, ecolor=markeredge,
                        markerfacecolor=markerface, markeredgecolor=markeredge)
-        ax[0].plot(x,func(x,*popt),c='r')
+        if len(x)<50:
+            start = x[0]
+            finish = x[len(x)-1]
+            u = np.linspace(start,finish,50)
+        else:
+            u = x
+        ax[0].plot(u,func(u,*popt),c='r')
         ax[1].errorbar(x, r, sigy, fmt=marker, ecolor=markeredge,
                        markerfacecolor=markerface, markeredgecolor=markeredge)
         
@@ -57,7 +65,13 @@ def Plot(x, y, sigy=None, func: 'function'=None, p0=None,
         fig,ax = plt.subplots(figsize=(8,6))
                 
         ax.scatter(x,y,marker=marker,facecolors=markerface,edgecolors=markeredge,s=markersize)
-        ax.plot(x,func(x,*popt),c='r')
+        if len(x)<50:
+            start = x[0]
+            finish = x[len(x)-1]
+            u = np.linspace(start,finish,50)
+        else:
+            u = x
+        ax.plot(u,func(u,*popt),c='r')
 
         # scales types
         if log:
@@ -69,11 +83,39 @@ def Plot(x, y, sigy=None, func: 'function'=None, p0=None,
     plt.xlabel(xlabel,size=12)
                   
     return (ax,popt,pcov)
+
+def Plot(x, y, sigy=None, xlabel: str='X', ylabel: str='Y', log: bool=False, 
+         marker: str='s', markersize: float=12, markeredge: str='k', markerface: str='none'):
+    
+    if sigy!=None:
+        fig,ax = plt.subplots()
+        ax.errorbar(x, y, sigy, fmt=marker, ecolor=markeredge,
+                       markerfacecolor=markerface, markeredgecolor=markeredge)
+        # scales types
+        if log:
+            ax.set_xscale('log')
+          
+        ax.grid()
+        ax.set_ylabel(ylabel,size=12)
         
+    else:
+        fig,ax = plt.subplots(figsize=(8,6))
+                
+        ax.scatter(x,y,marker=marker,facecolors=markerface,edgecolors=markeredge,s=markersize)
+        
+        # scales types
+        if log:
+            ax.set_xscale('log')
+    
+        ax.grid()
+        ax.set_ylabel(ylabel,size=12)
+    
+    plt.xlabel(xlabel,size=12)
+                  
+    return ax
+
 def Gradient(point: 'list|tuple', func: 'function', divs: int=1000):
-    '''
-        Returns the Gradient of a function at a specific point
-    '''
+    '''Returns the Gradient of a function at a specific point.'''
     argc = len(point)
 
     M = np.array( [np.linspace(var-0.1*var, var+0.1*var, divs) for var in point] )
@@ -92,7 +134,21 @@ def Gradient(point: 'list|tuple', func: 'function', divs: int=1000):
     return diff
 
 def Propagate_sig(vals: 'list|tuple', sigs: 'list|tuple', func: 'function'):
-  ''' Propagate Uncertainties '''
+  ''' Propagate Uncertainties. Receives 2 lists/tuples of values representing
+    the point/uncertainty, and the function relating the variables.
+
+    Example: 
+    
+    f(x, y, z) receives the tuple vals=(0.2, 1, 3.75) and sigs=(0.05, 0.1, 0.32) where,
+
+    x = 0.2+-0.05
+   
+    y = 1+-0.1
+
+    z = 3.75+-0.32
+
+    returns the uncertainty of f(0.2, 1, 3.75).  
+  '''
   
   grad = Gradient(vals,func)
   sigs = np.array(sigs)
@@ -100,8 +156,43 @@ def Propagate_sig(vals: 'list|tuple', sigs: 'list|tuple', func: 'function'):
 
   return sigf
 
-def Z_score(x: float, mu: float, sigx=float, sigmu: float=0):
+def Z_score(x: float, mu: float, sigx: float, sigmu: float=0):
+  '''Returns The Z-score of X'''
   sig = (sigx**2 + sigmu**2)**0.5
   Z = abs(x-mu)/sig
 
   return Z
+
+def FFTPlot(x, y, ylabel: str="FFT Amplitude", xlabel: str='Frequency'):
+    N = len(x)
+    Fs = N/(x[N-1]-x[0])
+    Yk = fft(y)
+
+    A = abs(Yk[:N//2+1])/(N//2)
+    freq = np.arange(0,Fs/2,Fs/N)
+
+    peaks_index, _ = find_peaks(A, prominence=0.2*max(A))
+    fpeaks = np.array(freq[peaks_index])
+    Apeaks = np.array(A[peaks_index])
+
+    width,height,left,right = peak_widths(A,peaks_index,rel_height=0.5)
+    right = right*Fs/N
+    left = left*Fs/N
+    width = right-left
+    annots = [f"${round(f,3)}$" for f in fpeaks]
+ 
+
+    fig, ax = plt.subplots(figsize=(8,6))
+    ax.plot(freq,A,c='k')
+    ax.plot(fpeaks, Apeaks+0.015*Apeaks,"vr")
+    for i, txt in enumerate(annots):
+        ax.annotate(txt, (fpeaks[i], Apeaks[i]+0.03*Apeaks),fontsize=12,ha='center')
+    ax.hlines(height,left,right, color="C3")
+
+
+    ax.grid()
+    ax.set_ylabel(ylabel,size=12)
+    ax.set_xlabel(xlabel,size=12)
+
+    return ax,fpeaks, [width,height,left,right]
+
